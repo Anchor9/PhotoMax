d04883006a81763bc0c5dfb94073cdbcab257af2
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-09-23 16:35:05 -0300

    FiMyImage class

---
+++ PhotoMax/MyImage.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using ExifLib;
+
+namespace PhotoMax
+{
+    public class MyImage
+    {
+        private int iso;
+        private float focalLength;
+        private string flash;
+        private double exposureTime;
+        private DateTime dataTime;
+        private string cammeraModel;
+        private string artist;
+        private string copyright;
+        private string geotag;
+        private int rank;
+        private string climate;
+        private bool animal;
+        private string path;
+        private int height;
+        private int width;
+        private string make;
+
+        public MyImage(string path)
+        {
+            ExifReader reader = new ExifReader(path);
+            Image file = Image.FromFile(path);
+            //Obtenemos height y width
+            Height = file.Height;
+
+            Width = file.Width;
+            // obtenemos ISO
+            int isos;
+            object isos1;
+            reader.GetTagValue<object>(ExifTags.PhotographicSensitivity, out isos1);
+            isos = (int)Convert.ToUInt64(isos1);
+
+            Iso = isos;
+            //focal
+            object fl;
+            reader.GetTagValue<object>(ExifTags.FocalLength, out fl);
+            double focal;
+            focal = Convert.ToDouble(fl);
+
+            FocalLength = (float)focal;
+
+            //exposure time
+            double et;
+            reader.GetTagValue(ExifTags.ExposureTime, out et);
+
+            ExposureTime = et;
+
+            string maker;
+            reader.GetTagValue(ExifTags.Make, out maker);
+            Make = maker;
+
+            DateTime datatime;
+
+            reader.GetTagValue(ExifTags.DateTime, out datatime);
+
+            DataTime = datatime;
+
+
+        }
+
+
+        public int Height
+        {
+            get => height;
+            set => height = value;
+        }
+
+        public int Width
+        {
+            get => width;
+            set => width = value;
+        }
+
+        public int Iso
+        {
+            get => iso;
+            set => iso = value;
+        }
+
+        public float FocalLength
+        {
+            get => focalLength;
+            set => focalLength = value;
+        }
+
+        public string Flash
+        {
+            get => flash;
+            set => flash = value;
+        }
+        public double ExposureTime
+        {
+            get => exposureTime;
+            set => exposureTime = value;
+        }
+        public DateTime DataTime
+        {
+            get => dataTime;
+            set => dataTime = value;
+        }
+        public string CammeraModel
+        {
+            get => cammeraModel;
+            set => cammeraModel = value;
+        }
+        public string Artist
+        {
+            get => artist;
+            set => artist = value;
+        }
+        public string Copyright
+        {
+            get => copyright;
+            set => copyright = value;
+        }
+        public string Geotag
+        {
+            get => geotag;
+            set => geotag = value;
+        }
+        public int Rank
+        {
+            get => rank;
+            set => rank = value;
+        }
+        public string Climate
+        {
+            get => climate;
+            set => climate = value;
+        }
+        public bool Animal
+        {
+            get => animal;
+            set => animal = value;
+        }
+        public string Make
+        {
+            get => make;
+            set => make = value;
+        }
+
+
+    }
+}
---
+++ PhotoMax/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using ExifLib;
+
+namespace PhotoMax
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            //Image image1 = Image.FromFile("/Users/joaquinbarrientos/Projects/PruebasProyecto/PruebasProyecto/DSC_0528.JPG");
+            //ExifReader reader = new ExifReader("/Users/joaquinbarrientos/Projects/PruebasProyecto/PruebasProyecto/DSC_0528.JPG");
+
+
+            //DateTime dataTime;
+            //reader.GetTagValue<DateTime>(ExifTags.DateTime, out dataTime);
+
+            //object iso1;
+            //int iso;
+            //reader.GetTagValue<object>(ExifTags.PhotographicSensitivity, out iso1);
+            //iso = (int)Convert.ToUInt64(iso1);
+            //Console.WriteLine(is);
+            //Console.WriteLine(dataTime);
+
+            string path = "/Users/joaquinbarrientos/Desktop/Universidad/6.Sexto Semestre/POO/PhotoMax/PhotoMax/Pintua.jpg";
+
+            ExifReader reader = new ExifReader(path);
+
+            //Obtenemos height y width
+
+            // obtenemos ISO
+            int isos;
+            object isos1;
+            reader.GetTagValue<object>(ExifTags.PhotographicSensitivity, out isos1);
+            isos = (int)Convert.ToUInt64(isos1);
+
+            Console.WriteLine(isos);
+            //focal
+            object fl;
+            reader.GetTagValue<object>(ExifTags.FocalLength, out fl);
+            double focal;
+            focal = Convert.ToDouble(fl);
+            Console.WriteLine(focal);
+
+
+            //exposure time
+            double et;
+            reader.GetTagValue(ExifTags.ExposureTime, out et);
+            Console.WriteLine(et);
+
+            string make;
+
+            reader.GetTagValue(ExifTags.Make, out make);
+            Console.WriteLine(make);
+
+            DateTime datatime;
+            
+            reader.GetTagValue(ExifTags.DateTime, out datatime);
+
+
+            string artist;
+
+            reader.GetTagValue(ExifTags.Artist, out artist);
+            Console.WriteLine(artist);
+
+
+
+
+
+        }
+
+    }
+}
